ac81fffaf4c97d7b93ca067747531f226a6d3ec9
Ronan Rocha <ronan.rochasouza@gmail.com>
2024-04-26 01:11:18 -0300

    dockerize application and add project web

--- DesafioPaschoalotto.Api/Program.cs
+++ DesafioPaschoalotto.Api/Program.cs
@@ -11,6 +11,7 @@ DependencyInjection.AddInfrastructure(builder.Services, builder.Configuration);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options => options.AddPolicy("CorsPolicy", c => c.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod()));
 
 var app = builder.Build();
 
@@ -20,12 +21,12 @@ using (var serviceScope = app.Services.GetService<IServiceScopeFactory>().Create
     context.Database.Migrate();
 }
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+
+app.UseSwaggerUI();
+
+app.UseCors("CorsPolicy");
 
 app.UseHttpsRedirection();
 
